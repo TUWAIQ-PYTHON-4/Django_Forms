6ef8be850b38f7af716579696aeb5a6a9e744387
Abdulhadi <a-ahmasani@hotmail.com>
2022-05-26 18:37:22 +0300

    addLab

---
+++ HW/PMProject/PMApp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Task
+
+
+def home(request):
+    x = Task.objects.all()
+    context = []
+    for i in x:
+        context.append(i)
+    return render(request, "home.html", {'context': context})
+
+
+def form_example(request):
+    return render(request, "form-example.html")
+
+
+
