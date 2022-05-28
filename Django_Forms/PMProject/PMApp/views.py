3213325d0b458e7bb46cb2af3930be6cd57a4ecd
Waleedoff <77509215+Waleedoff@users.noreply.github.com>
2022-05-28 20:09:17 +0300

    Add files via upload

---
+++ Django_Forms/PMProject/PMApp/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Task
+
+
+def admin_object(request):
+    if request.method == "POST":
+        form = PMForm(request.POST)
+    else:
+        form = PMForm()
+
+    if request.method == "POST":
+        form = PMForm(request.POST)
+    if form.is_valid():
+        for name, value in form.cleaned_data.items():
+            print("{}: ({}) {}".format(name, type(value), value))
+
+    x = Task.objects.all()
+    Tasks = []
+    for i in x:
+        Tasks.append(i)
+
+    for name in request.POST:
+        print("{}: {}".format(name, request.POST.getlist(name)))
+
+    return render(request, "base.html", {"Tasks": Tasks, 'method':request.method  })
+
+def form (request):
+    if request.method == "POST":
+        form = PMForm(request.POST)
+    else:
+        form = PMForm()
+
+    if request.method == "POST":
+        form = PMForm(request.POST)
+    if form.is_valid():
+        for name, value in form.cleaned_data.items():
+            print("{}: ({}) {}".format(name, type(value), value))
+
+    return render(request, 'form.html' , {'form':form})
